472d731d4586206d17f9d3ee75550fddb5661202
Thiago Ramos <thiagoramos.al@gmail.com>
2015-04-28 14:55:27 -0300

    First commit

---
+++ rails_box.rb
@@ -0,0 +1,12 @@
+puts 'Digite o nome do Sistema:'
+sistema = gets
+
+files = ['railsbox/development/Vagrantfile_template', 'railsbox/ansible/group_vars/all/config_template.yml']
+
+files.each do |file_name|
+  file = File.read(file_name)
+  new_content = file.gsub(/{{sistema}}/, sistema.strip)
+  new_file = file_name.gsub(/_template/, '')
+
+  File.open(new_file, 'w') { |file| file.write(new_content) }
+end
